acacfe8864bcb7f651e65447ebd6b71688154049
ANnamdi <mikeadossi@gmail.com>
2015-09-23 20:27:47 -0500

    first commit

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,8 +1,10 @@
 var user = new UserModel();
 var App = Backbone.Router.extend({
 	routes: {
-		'': 'profile',
-		'edit': 'edit'
+		'': 'profile', // what goes into the url is on the left.
+		'edit': 'edit',
+		'home': 'home',
+
 	},
 	profile: function() {
 		$('.page').hide();
@@ -11,8 +13,30 @@ var App = Backbone.Router.extend({
 	edit: function() {
 		$('.page').hide();
 		$('#edit').show();
+		edit();
+	},
+	home: function() {
+		$('.page').hide();
+		$('#home').show();
 	}
 });
 
 var app = new App();
 Backbone.history.start();
+
+function edit(){
+	$('#name').val(user.get('name'));
+	$('#inputEmail3').val(user.get('email'));
+	$('#role').val(user.get('role'));
+	$('#inputPassword3').val(user.get('password'));
+}
+
+	$('#saveButton').on('click', function(){
+		user.set('name',$('#name').val()); 
+		user.set('email',$('#inputEmail3').val());
+		user.set('role',$('#role').val());
+		user.set('password',$('#inputPassword3').val()); 
+	})
+
+
+
